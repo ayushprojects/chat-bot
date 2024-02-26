97d3cd21c01208dadeeb405e10d2d32feab902b6
Ayush Namdev <ayushnamdev375@gmail.com>
2024-02-26 17:56:22 +0530

    commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function App() {
+  return (
+    <View className="bg-red-200">
+      <Text>App</Text>
+    </View>
+  );
+}
